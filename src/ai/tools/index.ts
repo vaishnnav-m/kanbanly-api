e65f6dfc6266705e7dd00abd7b5f9232296c732f
vaishnnav-m <vaishnnav0@gmail.com>
2025-12-23 13:01:06 +0530

    fix: fix bug in task creation tool

--- src/ai/tools/index.ts
+++ src/ai/tools/index.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { SearchDocumentationTool } from "./retrieval.tool";
 import { CreateProjectTool } from "./create-project.tool";
+import { CreateTaskTool } from "./create-task.tool";
+import { ToolPayloadDto } from "../../types/dtos/ai/ai.dto";
 
 @injectable()
 export class ToolFactory {
@@ -8,13 +10,15 @@ export class ToolFactory {
     @inject(SearchDocumentationTool)
     private _searchDocTool: SearchDocumentationTool,
     @inject(CreateProjectTool)
-    private _createProjectTool: CreateProjectTool
+    private _createProjectTool: CreateProjectTool,
+    private _createTaskTool: CreateTaskTool
   ) {}
 
-  build(workspaceId: string, userId: string) {
+  build({ workspaceId, userId, projectId }: ToolPayloadDto) {
     return [
       this._searchDocTool.build(),
       this._createProjectTool.build({ workspaceId, userId }),
+      this._createTaskTool.build({ workspaceId, userId, projectId }),
     ];
   }
 }
